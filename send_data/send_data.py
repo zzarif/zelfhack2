0d31dd8f4b3c0ad4bb9c782c4e2826309b9b72da
Zibran Zarif <zzamio1112@gmail.com>
2024-10-05 17:07:54 +0600

    Post API added

--- send_data/send_data.py
+++ send_data/send_data.py
@@ -1,5 +1,5 @@
 import pandas as pd
-# import requests
+import requests
 import os
 
 if __name__ == "__main__":
@@ -7,14 +7,13 @@ if __name__ == "__main__":
 
     for index, row in df.iterrows():
 
-        # Call API here
-        print(
-            row['keyword'],
-        row['video_url'],
-        row['video_caption'],
-        row['author_username'],
-        row['author_url'],
-        row['following_count'],
-        row['followers_count'],
-        row['likes_count']
-        )
+        requests.post("https://localhost:8000/api/v1/contents/", {
+            "keyword": row['keyword'],
+            "video_url": row['video_url'],
+            'video_caption': row['video_caption'],
+            'author_username': row['author_username'],
+            'author_url': row['author_url'],
+            'following_count': row['following_count'],
+            'followers_count': row['followers_count'],
+            'likes_count': row['likes_count']
+        })
